019479e33e4dd4872f2bbd0e7143a026d2de47d1
indivioM <indivio@yandex.ru>
2020-09-08 23:01:31 +0300

    BCRYPT LOGIN

---
+++ src/main/java/com/geekbrains/geekmarket/services/ProductService.java
@@ -0,0 +1,40 @@
+package com.geekbrains.geekmarket.services;
+
+import com.geekbrains.geekmarket.entities.Product;
+import com.geekbrains.geekmarket.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        Page<Product> pr = productRepository.findAll(PageRequest.of(0,10));
+        System.out.println(pr);
+        return pr.stream().collect(Collectors.toList());
+    }
+
+    public List<Product> getProductsByVendorCode(String code) {
+        return productRepository.findAllByVendorCode(code);
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            return product.get();
+        }
+        return null;
+    }
+}
